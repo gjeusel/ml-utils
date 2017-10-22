6237dd4233e7a164b7e039f423bc83d2495e9002
Guillaume <guillaume.jeusel@gmail.com>
2017-10-22 18:26:39 +0200

    Writting the setup.py
    Signed-off-by: Guillaume <guillaume.jeusel@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='ml-utils',
+      version='1.0',
+      description='Common tools for Machine Learning',
+      author='Guillaume Jeusel',
+      author_email='guillaume.jeusel@gmail.com',
+      packages=['ml_utils'],
+     )
